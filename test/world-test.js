164ce23deb9b4955f8fbcab5472d57036554b744
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-11 16:33:43 -0600

    add unit tests for tray test default values test given values for tray test move right test move left refactor right arrow and left arrow into the World all tray tests passing start world unit test

---
+++ test/world-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const World = require('../lib/world');
+
+describe('World', function(){
+  context('with default attributes', function(){
+    it('should move to the left with left arrow keypress', function(){
+      let world = new World();
+      assert.equal(world.tray.x, 250);
+      world.leftArrow();
+      assert.isBelow(world.tray.x, 250);
+      assert.isAbove(world.tray.x, 0);
+    });
+
+    it('should move to the right with right arrow keypress', function(){
+      let world= new World();
+      assert.equal(world.tray.x, 250);
+      world.rightArrow();
+      assert.isAbove(world.tray.x, 250);
+      assert.isBelow(world.tray.x, 500);
+    });
+
+  });
+});
