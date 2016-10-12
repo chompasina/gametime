cdab296f582b8b16275b0cee00cc7ee15033641d
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-12 15:41:26 -0600

    add start for healthFood object add start for healthFood test add start for healthFood fallDown function add start for drawHealthFood and moveHealthFood

---
+++ test/health_food-test.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const healthFood = require('../lib/healthFood.js');
+
+describe('healthFood', function(){
+  context('with default attributes', function(){
+    var healthFood = new healthFood();
+    it('should be instantiated', function(){
+      assert.isObject(healthFood);
+    });
+
+    it('should have an x-coordinate', function(){
+      assert.equal(healthFood.x, 500);
+    });
+
+    it('should have a y-coordinate', function(){
+      assert.equal(healthFood.y, 10);
+    });
+
+    it('should have a height', function(){
+      assert.equal(healthFood.height, 20);
+    });
+
+    it('should have a width', function(){
+      assert.equal(healthFood.width, 20);
+    });
+
+    it('should have a speed', function(){
+      assert.equal(healthFood.speed, 2);
+    });
+  });
+
+  context('with given attributes', function(){
+    it('can accept a new y-coordinate', function(){
+      assert.equal(healthFood.y, 50);
+    });
+  });
+});
