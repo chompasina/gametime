90450155fc11d2caf3dc00d267c14ed2a4c4a165
Karina Gonzalez <karinamzalez@gmail.com>
2016-10-11 19:36:28 -0600

    add world instantion tests * add overlord to the world

--- test/world-test.js
+++ test/world-test.js
@@ -5,6 +5,28 @@ const World = require('../lib/world');
 
 describe('World', function(){
   context('with default attributes', function(){
+    it('should be instantiated', function(){
+      let world = new World();
+      assert.isObject(world);
+    });
+    it('should have a width', function(){
+      let world = new World();
+      assert.equal(world.width, null);
+    });
+    it('should have a height', function(){
+      let world = new World();
+      assert.equal(world.height, null);
+    });
+    it('should have a tray', function(){
+      let world = new World();
+      assert.isObject(world.tray);
+    });
+    it('should have an overlord', function(){
+      let world = new World();
+      assert.isObject(world.overlord);
+    });
+  });
+  context('movement', function(){
     it('should move to the left with left arrow keypress', function(){
       let world = new World();
       assert.equal(world.tray.x, 250);
