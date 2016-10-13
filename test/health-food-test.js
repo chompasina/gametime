d02f335025060a602f8972814aa0cd0130fc4d20
Tommasina <tommasina1@gmail.com>
2016-10-12 18:20:50 -0600

    Adds function to randomize health foods - Created function dropRandomFood - Created function moveHealthFood which loops through our random health foods - Working on refactor out of index.js - Working on writing test for new functions

--- test/health-food-test.js
+++ test/health-food-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const HealthFood = require('../lib/health-food.js');
+const World = require('../lib/world.js');
 
 describe('healthFood', function(){
   context('with default attributes', function(){
@@ -28,7 +29,7 @@ describe('healthFood', function(){
     });
 
     it('should have a speed', function(){
-      assert.equal(healthFood.speed, 2);
+      assert.equal(healthFood.speed, 0.5);
     });
   });
 
@@ -37,6 +38,16 @@ describe('healthFood', function(){
       let healthFood = new HealthFood({y: 50});
       assert.equal(healthFood.y, 50);
     });
-
   });
+  // context('movement', function(){
+  //   it('moves down the canvas', function(){
+  //     let world = new World(600, 600);
+  //     let healthFood = new HealthFood();
+  //     assert.equal(healthFood.x, 500);
+  //     assert.equal(healthFood.y, 0);
+  //     healthFood.fallDown(world);
+  //     assert.equal(healthFood.x, 500);
+  //     assert.isAbove(healthFood.y, 0);
+  //   });
+  // });
 });
