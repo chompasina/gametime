699855b7227cc241f8f3949a5af26dee381e7338
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-17 15:04:49 -0600

    adds beet sprite

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,11 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loader: "style!css!sass" }
+      { test: /\.scss$/, loader: "style!css!sass" },
+      { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192"}
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.scss', '.css']
+    extensions: ['', '.js', '.json', '.scss', '.css', '.png']
   }
 };
