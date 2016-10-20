2c6847d4bcb524e7ef87bbd589fdb3631bb0cc93
Karina Gonzalez <karinamzalez@gmail.com>
2016-10-20 12:37:40 -0600

    remove jshint errors

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,1 @@
-const chai = require('chai');
-const assert = chai.assert;
-const stub = require('./support/stub');
 require ('./index');
-const HealthFood = require('../lib/health-food');
-const RandomHealthFood = require('../lib/random-health-food');
